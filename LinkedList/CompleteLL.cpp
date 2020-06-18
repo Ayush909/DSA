e6efc4d592301e92e88afa0af549487d20f36fed
Ayush909 <kushwahaayu9@gmail.com>
2020-06-18 11:58:03 +0530

    completeLL

---
+++ LinkedList/CompleteLL.cpp
@@ -0,0 +1,67 @@
+//5->10->13->24->34
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};
+struct Node* head; //global variable
+
+//insert at beginning
+void prepend(int value){
+  Node* newNode = new Node();
+  newNode->data = value;
+  newNode->next = head;
+  head = newNode;
+}
+//printting the entire list
+void printList(){
+  Node* temp = head;
+  cout<<"List is : ";
+  while( temp != NULL){
+    cout<<temp->data<<" ";
+    temp = temp->next;
+  }
+  cout<<endl;
+}
+//traversing a the LL
+Node* traverse(int index){
+  int counter = 0;
+  Node* temp = head;
+  while(counter != index){
+    temp = temp->next;
+    counter++;
+  }
+  return temp;
+}
+//Insert at nth position in LL
+void InsertAtNth(int index, int value){
+  Node* newNode = new Node();
+  newNode->data = value;
+  newNode->next = NULL;
+  if(index == 0){
+    return prepend(value);
+  }
+  Node* leader = traverse(index-1);
+  Node* handler = leader->next;
+  leader->next = newNode;
+  newNode->next = handler;
+
+}
+
+int main(){
+   
+    head = NULL;
+    
+    prepend(34);    
+    prepend(24);    
+    prepend(13);    
+    prepend(10);    
+    prepend(5);
+    printList(); 
+    InsertAtNth(0, 99);  
+    printList(); 
+
+    return 0 ;
+}
