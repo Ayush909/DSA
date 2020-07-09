80a717607ee630c464e0bc92c5a05138c38eb5ad
Ayush909 <kushwahaayu9@gmail.com>
2020-07-09 12:16:53 +0530

    rotatearray-method2

--- Arrays/rotateArray.cpp
+++ Arrays/rotateArray.cpp
@@ -42,3 +42,54 @@ int main()
 }
 
 
+Method 2 : Rotate one by one
+
+#include <iostream>    // to read and write data
+#include <cstdlib>
+#include <string>
+#include <limits> //to show min max of data types
+#include <vector>
+#include <sstream>
+#include <numeric>
+#include <ctime>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+  int n = 7;
+  int arr[n] = {1,2,3,4,5,6,7};
+
+  int d =2;
+  int temp;
+
+  //main steps here set temp = first element of array and shift all the ele one by one to left.
+  // at last add arr[n-1] = temp;
+  for(int i = 0 ;i < d ; i++){ // this loop is to run the loop to rotate arr by 2 steps
+        temp = arr[0];
+    for(int j =0;j<n;j++){
+        arr[j] = arr[j+1];
+    }
+        arr[n-1] = temp;
+  }
+
+  //prints the result
+  for(int i=0;i<n;i++){
+    cout<<arr[i]<<" ";
+  }
+
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
